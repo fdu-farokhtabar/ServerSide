4092775e1c10f583e84620d1f57f86512cb71749
ComeonLu <s.lu1@student.fdu.edu>
2024-06-15 16:58:01 -0700

    Add files via upload

---
+++ Domain/Entity/Filter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Domain.Entity
+{
+    public class Filter
+    // Filter means Filter data or specific processing conditions， for example in "chair" group we can choose filter "wooden"
+    {
+        public Guid Id { get; set; } //Primary Key
+        public string Name { get; set; }
+        public int Order { get; set; }
+
+        // [Tag][Tag][Tag]... Excel Format
+        // ["Tag","Tag","Tag"] json => Db Format
+        public string Tags { get; set; }
+
+
+        // [Group],[Group],[Group]
+        ///["Group","Group","Group"] json => Db Format
+        public string Groups { get; set; }
+    }
+}
---
+++ Domain/Entity/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain.Entity
+{
+    public class Group
+    // In certain situations, customers need to display products within specific categories or divisions that do not belong to them.
+    // For example 
+    // Situation1: A product belongs to Category A, but the customer wants it to be displayed in Category B as well. The Group class can be used to achieve this cross-category display.
+    // Situation2: Customers can group products according to specific needs to display them in different contexts. This flexibility allows products to be displayed without being restricted to a single category.
+    {
+        public Guid Id { get; set; } // primary key
+        public string Name { get; set; }
+        public bool IsVisible { get; set; } 
+        public int Order { get; set; }
+    }
+}
