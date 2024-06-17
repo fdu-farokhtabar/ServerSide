49443b6c2d508275db565807824baa51c863f7d1
ComeonLu <s.lu1@student.fdu.edu>
2024-06-16 21:35:21 -0700

    Add files via upload

---
+++ Application/Data/Config/FilterConfig.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Application.Data.Config
+{
+    public class FilterConfig : IEntityTypeConfiguration<Filter>
+    {
+        public void Configure(EntityTypeBuilder<Filter> builder)
+        {
+            builder.ToTable(nameof(Filter)); //map to a real table "Filter"
+            builder.HasKey(x => x.Id); // Primary Key
+            builder.Property(x => x.Name).IsRequired(true).HasMaxLength(200);
+            builder.Property(x => x.Tags).IsRequired(true);
+            builder.Property(x => x.Groups).IsRequired(false);
+            builder.Property(x => x.Order).IsRequired(true).HasDefaultValue(0);
+
+            builder.HasIndex(x => x.Name).IsUnique(); 
+            builder.HasIndex(x => x.Order);
+        }
+    }
+}
---
+++ Application/Data/Config/GroupConfig.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Application.Data.Config
+{
+    public class GroupConfig : IEntityTypeConfiguration<Group>
+    {
+        public void Configure(EntityTypeBuilder<Group> builder)
+        {
+            builder.ToTable(nameof(Group)); // Map to a real table "Group"
+            builder.HasKey(x => x.Id); // Primary Key
+            builder.Property(x => x.Name).IsRequired(true).HasMaxLength(200);
+            builder.Property(x => x.IsVisible).IsRequired(true).HasDefaultValue(0);
+            builder.Property(x => x.Order).IsRequired(true).HasDefaultValue(0);
+
+            builder.HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
