41f14c3124bb026d4bc6896df78a8316591a097a
MaggieMengXingchen <138932193+MaggieMengXingchen@users.noreply.github.com>
2024-06-16 22:01:54 -0700

    add Setting Config & Tutorial Config

---
+++ Application/Data/Config/SettingConfig.cs
@@ -0,0 +1,17 @@
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Application.Data.Config
+{
+    public class SettingConfig : IEntityTypeConfiguration<Setting>
+    {
+        public void Configure(EntityTypeBuilder<Setting> builder)
+        {
+            builder.ToTable(nameof(Setting));
+            builder.HasKey(x => x.Key);
+            builder.Property(x => x.Key).IsRequired(true).HasMaxLength(200);
+            builder.Property(x => x.Value).IsRequired(true);
+        }
+    }
+}
